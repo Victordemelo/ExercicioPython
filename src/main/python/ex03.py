a4adf59f72a781f36ddf614257846e517632c02b
victor <victorgameplays535@gmail.com>
2025-04-26 11:03:06 -0300

    Feat: Adicionando o exercicio 03.

---
+++ src/main/python/ex03.py
@@ -0,0 +1,11 @@
+#Objetivo: Criar um programa que leia dois números e mostre a soma entre eles.
+#Autor: Victor M
+
+print("Digite um valor: ")
+n1 = int(input())
+print("Digite o segundo valor: ")
+n2 = int(input())
+
+soma = n1+n2
+
+print("A soma entre {} e {} é igual a = {}".format(n1,n2,soma))
