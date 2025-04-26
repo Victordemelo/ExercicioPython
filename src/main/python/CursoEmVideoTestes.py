c537e679c2537214ac94090962f9cde19e52ed2a
victor <victorgameplays535@gmail.com>
2025-04-26 10:37:42 -0300

    Feat: adicionando o desafio 04 da aula.

---
+++ src/main/python/CursoEmVideoTestes.py
@@ -0,0 +1,48 @@
+#Introdução ao Python#
+#Autor: Victor M#
+
+
+
+#Desafio 01#
+
+#print("Digite o seu nome: ")
+#nome = input()
+
+#print("Olá", nome , "Prazer em te conhecer!")
+
+
+
+#Desafio 02#
+
+#print("Digite o dia do seu nascimento: ")
+#dia = input()
+#print("Digite o Mês do seu nascimento: ")
+#mes = input()
+#print("Digite o Ano do seu nascimento: ")
+#ano = input()
+
+#print("Seu aniversário é:" , dia, "/" , mes , "/" , ano)
+
+
+
+#Desafio 03#
+
+#print("Digite o primeiro numero: ")
+#n1 = int(input())
+#print("Digite o Segundo numero: ")
+#n2 = int(input())
+
+#soma = n1+n2
+#print("A soma entre:", n1, "e", n2, "vale =" , soma)                   Python 2.0
+#OU
+#print("A soma entre {} e {} vale = {}".format(n1,n2,soma))              Python 3.0
+
+
+
+#Desafio 04#
+#print("Digite Algo: ")
+#n = input()
+
+#print("Alpha : ", n.isalpha())
+#print("Numério: ", n.isnumeric())
+#print("Alpha Numério: ", n.isalnum())
