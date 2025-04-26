24a353111a17fa97adfca48fa3af963db75e833e
victor <victorgameplays535@gmail.com>
2025-04-26 09:57:25 -0300

    Feat: adicionando exercicio 01 e 02.

---
+++ src/main/python/ex01.py
@@ -0,0 +1,5 @@
+#Objetivo: Crie um programa que escreva "Olá, mundo" na tela.
+#Autor: Victor M
+
+mensagem = "Olá, mundo!"
+print(mensagem)
---
+++ src/main/python/ex02.py
@@ -0,0 +1,7 @@
+#Objetivo: Digitar o nome da pessoa, e fazer aparecer uma mensagem de bem-vindo para ela.
+#Autor: Victor M
+
+print("Digite o seu nome: ")
+nome = input()
+
+print("É um prazer te conhecer,", nome)
