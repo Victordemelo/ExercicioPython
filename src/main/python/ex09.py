f7a3d51308fcbcfbc78655ed098508e13feced76
victor <victorgameplays535@gmail.com>
2025-04-27 10:19:23 -0300

    Feat: Adicionando o exercício 09.

---
+++ src/main/python/ex09.py
@@ -0,0 +1,29 @@
+#Objetivo: Criar um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+#Autor: Victor M
+
+print("Digite um numero para ver a tabuada: ")
+numero = int(input())
+
+um = numero*1
+dois = numero*2
+tres = numero*3
+quatro = numero*4
+cinco = numero*5
+seis = numero*6
+sete = numero*7
+oito = numero*8
+nove = numero*9
+dez = numero*10
+
+print("------------")
+print("{} x 1 = {}".format(numero,um))
+print("{} x 2 = {}".format(numero,dois))
+print("{} x 3 = {}".format(numero,tres))
+print("{} x 4 = {}".format(numero,quatro))
+print("{} x 5 = {}".format(numero,cinco))
+print("{} x 6 = {}".format(numero,seis))
+print("{} x 7 = {}".format(numero,sete))
+print("{} x 8 = {}".format(numero,oito))
+print("{} x 9 = {}".format(numero,nove))
+print("{} x 10 = {}".format(numero,dez))
+print("------------")
