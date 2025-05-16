19ef9a0211879508a4c78b5d47eda269a90835e1
victor <victorgameplays535@gmail.com>
2025-05-16 10:41:43 -0300

    Feat: Adicionando o desafio 21.

--- src/main/python/CursoEmVideoTestes.py
+++ src/main/python/CursoEmVideoTestes.py
@@ -306,3 +306,15 @@
 
 #print("A ordem dos alunos ficou: ")
 #print(lista)
+
+
+
+
+#Desafio 21#
+
+
+#pygame.init()
+
+#pygame.mixer.music.load('C:/Users/victor/Codigos_Programação/vscode/Python1/ExercicioPython/src/main/python/ex21.mp3')
+#pygame.mixer.music.play()
+#pygame.time.wait(180000)
