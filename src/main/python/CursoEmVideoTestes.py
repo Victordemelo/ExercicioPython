d3d95aab1756429b92955fed8b7e70f6d1142ec9
victor <victorgameplays535@gmail.com>
2025-05-17 11:52:14 -0300

    Feat: Adicionando o Desafio 23.

--- src/main/python/CursoEmVideoTestes.py
+++ src/main/python/CursoEmVideoTestes.py
@@ -1,328 +1,284 @@
-#Introdução ao Python#
-#Autor: Victor M#
+# Introdução ao Python#
+# Autor: Victor M#
 
 
+# Desafio 01#
 
+# print("Digite o seu nome: ")
+# nome = input()
 
-#Desafio 01#
+# print("Olá", nome , "Prazer em te conhecer!")
 
-#print("Digite o seu nome: ")
-#nome = input()
 
-#print("Olá", nome , "Prazer em te conhecer!")
+# Desafio 02#
 
+# print("Digite o dia do seu nascimento: ")
+# dia = input()
+# print("Digite o Mês do seu nascimento: ")
+# mes = input()
+# print("Digite o Ano do seu nascimento: ")
+# ano = input()
 
+# print("Seu aniversário é:" , dia, "/" , mes , "/" , ano)
 
 
-#Desafio 02#
+# Desafio 03#
 
-#print("Digite o dia do seu nascimento: ")
-#dia = input()
-#print("Digite o Mês do seu nascimento: ")
-#mes = input()
-#print("Digite o Ano do seu nascimento: ")
-#ano = input()
+# print("Digite o primeiro numero: ")
+# n1 = int(input())
+# print("Digite o Segundo numero: ")
+# n2 = int(input())
 
-#print("Seu aniversário é:" , dia, "/" , mes , "/" , ano)
+# soma = n1+n2
+# print("A soma entre:", n1, "e", n2, "vale =" , soma)                   Python 2.0
+# OU
+# print("A soma entre {} e {} vale = {}".format(n1,n2,soma))              Python 3.0
 
 
+# Desafio 04#
+# print("Digite Algo: ")
+# n = input()
 
+# print("Alpha : ", n.isalpha())
+# print("Numério: ", n.isnumeric())
+# print("Alpha Numério: ", n.isalnum())
 
-#Desafio 03#
 
-#print("Digite o primeiro numero: ")
-#n1 = int(input())
-#print("Digite o Segundo numero: ")
-#n2 = int(input())
+# Desafio 05#
+# print("Digite um valor: ")
+# n1 = int(input())
+# print("Digite outro valor: ")
+# n2 = int(input())
 
-#soma = n1+n2
-#print("A soma entre:", n1, "e", n2, "vale =" , soma)                   Python 2.0
-#OU
-#print("A soma entre {} e {} vale = {}".format(n1,n2,soma))              Python 3.0
+# s = n1+n2
+# m = n1*n2
+# d = n1/n2
+# di = n1//n2
+# exp = n1**n2
 
+# print("A soma é {},\nA multiplicação é {}, \nA divisão é {:.3f}". format(s, m, d), end=" ")
+# print("A divisão inteira é {}, a potência é {}".format(di, exp))
 
 
+# Desafio 06#
+# print("Digite um valor: ")
+# n1 = int(input())
+# print("Digite outro valor: ")
+# n2 = int(input())
 
-#Desafio 04#
-#print("Digite Algo: ")
-#n = input()
+# soma = n1+n2
+# sucessor = (n1+n2)+1
+# antecessor = (n1+n2)-1
 
-#print("Alpha : ", n.isalpha())
-#print("Numério: ", n.isnumeric())
-#print("Alpha Numério: ", n.isalnum())
+# print("O Valor da soma foi {}, o sucessor é {}, o antecessor é {}.".format(soma, sucessor, antecessor))
 
 
+# Desafio 07#
+# print("Digite um número: ")
+# n1 = int(input())
 
+# dobro = n1*2
+# triplo = n1*3
+# raiz = n1/(1/2)
 
-#Desafio 05#
-#print("Digite um valor: ")
-#n1 = int(input())
-#print("Digite outro valor: ")
-#n2 = int(input())
+# print("Número digitado foi: {}, o dobro do numero digitado: {}, o triplo: {}, e a raiz quadrada: {}.".format(n1, dobro, triplo, raiz))
 
-#s = n1+n2
-#m = n1*n2
-#d = n1/n2
-#di = n1//n2
-#exp = n1**n2
 
-#print("A soma é {},\nA multiplicação é {}, \nA divisão é {:.3f}". format(s, m, d), end=" ")
-#print("A divisão inteira é {}, a potência é {}".format(di, exp))
+# Desafio 08#
+# print("Digite a primeira nota: ")
+# nota1 = int(input())
+# print("Digite a segunda nota: ")
+# nota2 = int(input())
 
+# media = (nota1+nota2) / 2
 
+# print("A primeira nota foi: {}\nA segunda nota foi: {}\nA média do aluno foi: {}".format(nota1, nota2, media))
 
 
-#Desafio 06#
-#print("Digite um valor: ")
-#n1 = int(input())
-#print("Digite outro valor: ")
-#n2 = int(input())
+# Desafio 09#
+# print("Digite o valor em metros: ")
+# metros = float(input())
 
-#soma = n1+n2
-#sucessor = (n1+n2)+1
-#antecessor = (n1+n2)-1
+# centrimetros = metros * 100
+# milimetros = metros * 1000
 
-#print("O Valor da soma foi {}, o sucessor é {}, o antecessor é {}.".format(soma, sucessor, antecessor))
+# print("O valor digitado em metros: {}\nO valor digitado em centímetros: {}\nO valor digitado em milímetros: {}".format(metros, centrimetros, milimetros))
 
 
+# Desafio 10#
+# print("Digite um numéro inteiro para fazer a tabuada: ")
+# inteiro = int(input())
 
+# um = 1*inteiro
+# dois = 2*inteiro
+# tres = 3*inteiro
+# quatro = 4*inteiro
+# cinco = 5*inteiro
+# seis = 6*inteiro
+# sete = 7*inteiro
+# oito = 8*inteiro
+# nove = 9*inteiro
+# dez = 10*inteiro
 
-#Desafio 07#
-#print("Digite um número: ")
-#n1 = int(input())
+# print("1 x {} = {}".format(inteiro,um))
+# print("2 x {} = {}".format(inteiro,dois))
+# print("3 x {} = {}".format(inteiro,tres))
+# print("4 x {} = {}".format(inteiro,quatro))
+# print("5 x {} = {}".format(inteiro,cinco))
+# print("6 x {} = {}".format(inteiro,seis))
+# print("7 x {} = {}".format(inteiro,sete))
+# print("8 x {} = {}".format(inteiro,oito))
+# print("9 x {} = {}".format(inteiro,nove))
+# print("10 x {} = {}".format(inteiro,dez))
 
-#dobro = n1*2
-#triplo = n1*3
-#raiz = n1/(1/2)
 
-#print("Número digitado foi: {}, o dobro do numero digitado: {}, o triplo: {}, e a raiz quadrada: {}.".format(n1, dobro, triplo, raiz))
+# Desafio 11#
+# print("Digite quantos ($)Reais você tem em sua carteira: ")
+# reais = float(input())
 
+# dolar = reais / 3.27
 
+# print("Você pode comprar {:.2f} em dólares.".format(dolar))
 
 
-#Desafio 08#
-#print("Digite a primeira nota: ")
-#nota1 = int(input())
-#print("Digite a segunda nota: ")
-#nota2 = int(input())
+# Desafio 12#
+# print("Digite a Largura da parede: ")
+# largura = float(input())
+# print("Digite a Altura da parede: ")
+# altura = float(input())
 
-#media = (nota1+nota2) / 2
+# area = largura*altura
+# litros = area / 2
 
-#print("A primeira nota foi: {}\nA segunda nota foi: {}\nA média do aluno foi: {}".format(nota1, nota2, media))
+# print("A Área da parede é {:.2f} m²".format(area))
+# print("Será nescessário {:.2f} litros de tinta para pintar uma parede de {:.2f} m²".format(litros, area))
 
 
+# Desafio 13#
+# print("Digite o preço do produto: ")
+# preco = float(input())
 
+# desconto = (preco/100) * 5
+# precodesc = preco - desconto
 
-#Desafio 09#
-#print("Digite o valor em metros: ")
-#metros = float(input())
 
-#centrimetros = metros * 100
-#milimetros = metros * 1000
+# print("O Valor do produto sem desconto é: {:.2f} R$, com o desconto fica {:.2f} R$".format(preco, precodesc))
 
-#print("O valor digitado em metros: {}\nO valor digitado em centímetros: {}\nO valor digitado em milímetros: {}".format(metros, centrimetros, milimetros))
 
+# Desafio 14#
+# print("Digite o seu salário: ")
+# salario = float(input())
 
+# bonus = (salario/100) * 15
+# salariobonus = salario + bonus
 
+# print("Parebéns, o seu salário de {:.2f}, sera aumentado para {:.2f}".format(salario, salariobonus))
 
-#Desafio 10#
-#print("Digite um numéro inteiro para fazer a tabuada: ")
-#inteiro = int(input())
 
-#um = 1*inteiro
-#dois = 2*inteiro
-#tres = 3*inteiro
-#quatro = 4*inteiro
-#cinco = 5*inteiro
-#seis = 6*inteiro
-#sete = 7*inteiro
-#oito = 8*inteiro
-#nove = 9*inteiro
-#dez = 10*inteiro
+# Desafio 15#
 
-#print("1 x {} = {}".format(inteiro,um))
-#print("2 x {} = {}".format(inteiro,dois))
-#print("3 x {} = {}".format(inteiro,tres))
-#print("4 x {} = {}".format(inteiro,quatro))
-#print("5 x {} = {}".format(inteiro,cinco))
-#print("6 x {} = {}".format(inteiro,seis))
-#print("7 x {} = {}".format(inteiro,sete))
-#print("8 x {} = {}".format(inteiro,oito))
-#print("9 x {} = {}".format(inteiro,nove))
-#print("10 x {} = {}".format(inteiro,dez))
+# import math;
+# ou
+# from math import sqrt
 
 
+# print("Digite o numero para obter a raiz: ")
+# raiz = int(input())
 
+# raizquadrada = math.sqrt(raiz)
 
-#Desafio 11#
-#print("Digite quantos ($)Reais você tem em sua carteira: ")
-#reais = float(input())
+# print("A raiz quadrada é: {}".format(raizquadrada))
 
-#dolar = reais / 3.27
 
-#print("Você pode comprar {:.2f} em dólares.".format(dolar))
+# Desafio 16#
+# import math
 
+# print("Digite um número real: ")
+# real = float(input())
 
+# conversor = math.trunc(real)
 
+# print("O número real digitado foi {}, e a sua porção inteira é {}".format(real, conversor))
 
-#Desafio 12#
-#print("Digite a Largura da parede: ")
-#largura = float(input())
-#print("Digite a Altura da parede: ")
-#altura = float(input())
 
-#area = largura*altura
-#litros = area / 2
+# Desafio 17#
+# import math
 
-#print("A Área da parede é {:.2f} m²".format(area))
-#print("Será nescessário {:.2f} litros de tinta para pintar uma parede de {:.2f} m²".format(litros, area))
+# print("Digite o cateto oposto: ")
+# oposto = float(input())
+# print("Digite o cateto adjacente: ")
+# adjacente = float(input())
 
+# hipotenusa = math.hypot(oposto, adjacente)
 
+# print("O cateto oposto é {}, o cateto adjacente é {}, e a hipotenusa do triângulo retângulo é: {}".format(oposto, adjacente, hipotenusa))
 
 
-#Desafio 13#
-#print("Digite o preço do produto: ")
-#preco = float(input())
+# Desafio 18#
+# import math
+# print("Digite um ângulo qualquer: ")
+# angulo = float(input())
 
-#desconto = (preco/100) * 5
-#precodesc = preco - desconto
+# angulo_radiano = math.radians(angulo)
 
+# seno = math.sin(angulo_radiano)
+# cosseno = math.cos(angulo_radiano)
+# tangente = math.tan(angulo_radiano)
 
-#print("O Valor do produto sem desconto é: {:.2f} R$, com o desconto fica {:.2f} R$".format(preco, precodesc))
+# print("O Ângulo em graus foi {}, e o seno é {}, cosseno é {}, e a tangente é {}".format(angulo, seno, cosseno, tangente))
 
 
+# Desafio 19#
+# import random
 
+# print("Digite o nome dos quatro alunos")
+# print("Aluno 01: ")
+# aluno01 = input()
+# print("Aluno 02:")
+# aluno02 = input()
+# print("Aluno 03: ")
+# aluno03 = input()
+# print("Aluno 04: ")
+# aluno04 = input()
 
-#Desafio 14#
-#print("Digite o seu salário: ")
-#salario = float(input())
+# lista = [aluno01, aluno02, aluno03, aluno04]
 
-#bonus = (salario/100) * 15
-#salariobonus = salario + bonus
+# sorteio = random.choice(lista)
 
-#print("Parebéns, o seu salário de {:.2f}, sera aumentado para {:.2f}".format(salario, salariobonus))
+# print("O aluno escolhido foi {}".format(sorteio))
 
 
+# Desafio 20#
+# import random
+# print("Digite o nome dos quatro alunos")
+# print("Aluno 01: ")
+# aluno01 = str(input())
+# print("Aluno 02:")
+# aluno02 = str(input())
+# print("Aluno 03: ")
+# aluno03 = str(input())
+# print("Aluno 04: ")
+# aluno04 = str(input())
 
+# lista = [aluno01, aluno02, aluno03, aluno04]
 
-#Desafio 15#
+# random.shuffle(lista)
 
-#import math;
-#ou
-#from math import sqrt
+# print("A ordem dos alunos ficou: ")
+# print(lista)
 
 
-#print("Digite o numero para obter a raiz: ")
-#raiz = int(input())
+# Desafio 21#
 
-#raizquadrada = math.sqrt(raiz)
 
-#print("A raiz quadrada é: {}".format(raizquadrada))
+# pygame.init()
 
+# pygame.mixer.music.load('C:/Users/victor/Codigos_Programação/vscode/Python1/ExercicioPython/src/main/python/ex21.mp3')
+# pygame.mixer.music.play()
+# pygame.time.wait(180000)
 
 
-
-#Desafio 16#
-#import math
-
-#print("Digite um número real: ")
-#real = float(input())
-
-#conversor = math.trunc(real)
-
-#print("O número real digitado foi {}, e a sua porção inteira é {}".format(real, conversor))
-
-
-
-
-#Desafio 17#
-#import math
-
-#print("Digite o cateto oposto: ")
-#oposto = float(input())
-#print("Digite o cateto adjacente: ")
-#adjacente = float(input())
-
-#hipotenusa = math.hypot(oposto, adjacente)
-
-#print("O cateto oposto é {}, o cateto adjacente é {}, e a hipotenusa do triângulo retângulo é: {}".format(oposto, adjacente, hipotenusa))
-
-
-
-
-#Desafio 18#
-#import math
-#print("Digite um ângulo qualquer: ")
-#angulo = float(input())
-
-#angulo_radiano = math.radians(angulo)
-
-#seno = math.sin(angulo_radiano)
-#cosseno = math.cos(angulo_radiano)
-#tangente = math.tan(angulo_radiano)
-
-#print("O Ângulo em graus foi {}, e o seno é {}, cosseno é {}, e a tangente é {}".format(angulo, seno, cosseno, tangente))
-
-
-
-
-#Desafio 19#
-#import random
-
-#print("Digite o nome dos quatro alunos")
-#print("Aluno 01: ")
-#aluno01 = input()
-#print("Aluno 02:")
-#aluno02 = input()
-#print("Aluno 03: ")
-#aluno03 = input()
-#print("Aluno 04: ")
-#aluno04 = input()
-
-#lista = [aluno01, aluno02, aluno03, aluno04]
-
-#sorteio = random.choice(lista)
-
-#print("O aluno escolhido foi {}".format(sorteio))
-
-
-
-
-#Desafio 20#
-#import random
-#print("Digite o nome dos quatro alunos")
-#print("Aluno 01: ")
-#aluno01 = str(input())
-#print("Aluno 02:")
-#aluno02 = str(input())
-#print("Aluno 03: ")
-#aluno03 = str(input())
-#print("Aluno 04: ")
-#aluno04 = str(input())
-
-#lista = [aluno01, aluno02, aluno03, aluno04]
-
-#random.shuffle(lista)
-
-#print("A ordem dos alunos ficou: ")
-#print(lista)
-
-
-
-
-#Desafio 21#
-
-
-#pygame.init()
-
-#pygame.mixer.music.load('C:/Users/victor/Codigos_Programação/vscode/Python1/ExercicioPython/src/main/python/ex21.mp3')
-#pygame.mixer.music.play()
-#pygame.time.wait(180000)
-
-
-
-
-#Códigos de exemplos
+# Códigos de exemplos
 
 '''
 frase = 'Curso em Vídeo Python'
@@ -351,48 +307,72 @@ print(dividido[2][3])
 '''
 
 
+# Desafio 22#
+# print('Digite o seu nome: ')
+# nome = str(input())
 
+# nome_maisculo = nome.upper()
 
-#Desafio 22#
-#print('Digite o seu nome: ')
-#nome = str(input())
+# nome_minusculo = nome.lower()
 
-#nome_maisculo = nome.upper()
+# nome_espaco = nome.replace(" ", "")
+# nome_letras = len(nome_espaco)
 
-#nome_minusculo = nome.lower()
+# dividir =  nome.split()
+# letras_pri_nome = len(dividir[0])
 
-#nome_espaco = nome.replace(" ", "")
-#nome_letras = len(nome_espaco)
-
-#dividir =  nome.split()
-#letras_pri_nome = len(dividir[0])
-
-#print('Nome Maísculo: {}'.format(nome_maisculo))
-#print('Nome Minúsculo: {}'. format(nome_minusculo))
-#print('Quantidade de letras no nome sem espaço: {}'.format(nome_letras))
-#print('Quantas letras no primeiro nome: {}'.format(letras_pri_nome))
-
-
-
-
-#Desafio 23#
+# print('Nome Maísculo: {}'.format(nome_maisculo))
+# print('Nome Minúsculo: {}'. format(nome_minusculo))
+# print('Quantidade de letras no nome sem espaço: {}'.format(nome_letras))
+# print('Quantas letras no primeiro nome: {}'.format(letras_pri_nome))
 
 
 
 
-#Desafio 24#
+# Desafio 23#
+'''
+print('Digite um numero de 0 a 9999: ')
+n = input().zfill(4)
+
+n_unidade = n[3]
+n_dezena = n[2]
+n_centena = n[1]
+n_milhar = n[0]
+
+print('O Número digitado foi: {}'.format(n))
+print('A Unidade é: {}'.format(n_unidade))
+print('A Dezena é: {}'.format(n_dezena))
+print('A centena é: {}'.format(n_centena))
+print('O Milhar é: {}'.format(n_milhar))
+
+#OU
+
+print('Digite um numero de 0 a 9999: ')
+n = int(input())
+
+n_unidade = n // 1 % 10
+n_dezena = n // 10 % 10
+n_centena = n // 100 % 10
+n_milhar = n // 1000 % 10
+
+print('O Número digitado foi: {}'.format(n))
+print('A Unidade é: {}'.format(n_unidade))
+print('A Dezena é: {}'.format(n_dezena))
+print('A centena é: {}'.format(n_centena))
+print('O Milhar é: {}'.format(n_milhar))
+'''
+
+
+
+# Desafio 24#
 
 
 
 
-#Desafio 25#
+# Desafio 25#
 
 
+# Desafio 26#
 
 
-#Desafio 26#
-
-
-
-
-#Desafio 27#
+# Desafio 27#
